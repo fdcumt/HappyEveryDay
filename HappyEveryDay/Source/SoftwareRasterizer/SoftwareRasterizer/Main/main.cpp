861d1a60284468d5c0ff02c18cf3597a663c3e77
dufresnefu <dufresnefu@tencent.com>
2022-08-29 14:42:21 +0800

    1.添加software rasterizer目录 2.将CMake中SlnDir重命名为RootDir

---
+++ HappyEveryDay/Source/SoftwareRasterizer/SoftwareRasterizer/Main/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "MathTest/MathTest.h"
+#include "Test/UtilityTest.h"
+#include "Templates/UtilityTemplate.h"
+#include "Delegates/Delegate.h"
+
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
+
+
