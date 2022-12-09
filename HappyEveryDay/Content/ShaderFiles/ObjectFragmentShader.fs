3bb73597f5fde535bd828143c94bfc135ade221c
dufresnefu <dufresnefu@tencent.com>
2022-12-09 15:42:43 +0800

    使用phong光照

--- HappyEveryDay/Content/ShaderFiles/ObjectFragmentShader.fs
+++ HappyEveryDay/Content/ShaderFiles/ObjectFragmentShader.fs
@@ -2,8 +2,6 @@
 
 in vec3 Normal;  
 in vec3 FragPos;  
-in vec3 LightPosInViewSpace;  
-in vec3 LightingColor; 
 
 
 out vec4 FragColor;
@@ -22,21 +20,20 @@ uniform mat4 View;
 
 void main()
 {
-    //// cal ambient
-    //vec3 Ambint = LightColor*AmbientStrength;
-//
-    //// cal diffuse
-    //vec3 NormalizeNormal = normalize(Normal);
-    //vec3 LightDir = normalize(LightPosInViewSpace-FragPos);
-    //float DiffuseFactor = max(dot(NormalizeNormal, LightDir), 0.0);
-    //vec3 Diffuse = DiffuseFactor*LightColor;
-//
-    //// cal specular
-    //vec3 ViewDir = normalize(-FragPos);
-    //vec3 LightReflectDir = reflect(-LightDir, NormalizeNormal);
-    //float Spec = pow(max(dot(ViewDir, LightReflectDir), 0.0), 32);
-    //vec3 Specular = SpecularStrength*Spec*LightColor;
+    // cal ambient
+    vec3 Ambint = LightColor*AmbientStrength;
 
-    //FragColor = vec4((Ambint+Diffuse+Specular) * ObjectColor, 1.0);
-    FragColor = vec4(LightingColor* ObjectColor, 1.0);
+    // cal diffuse
+    vec3 NormalizeNormal = normalize(Normal);
+    vec3 LightDir = normalize(LightPos-FragPos);
+    float DiffuseFactor = max(dot(NormalizeNormal, LightDir), 0.0);
+    vec3 Diffuse = DiffuseFactor*LightColor;
+
+    // cal specular
+    vec3 ViewDir = normalize(ViewPos - FragPos);
+    vec3 LightReflectDir = reflect(-LightDir, NormalizeNormal);
+    float Spec = pow(max(dot(ViewDir, LightReflectDir), 0.0), 32);
+    vec3 Specular = SpecularStrength*Spec*LightColor;
+
+    FragColor = vec4((Ambint+Diffuse+Specular) * ObjectColor, 1.0);
 }
--- HappyEveryDay/Content/ShaderFiles/ObjectVertexShader.vs
+++ HappyEveryDay/Content/ShaderFiles/ObjectVertexShader.vs
@@ -7,45 +7,14 @@ uniform mat4 Model;
 uniform mat4 View;
 uniform mat4 Projection;
 
+
 out vec3 FragPos;
 out vec3 Normal;
-out vec3 LightPosInViewSpace;
-out vec3 LightingColor; 
-
-uniform float AmbientStrength;
-uniform float SpecularStrength;
-
-uniform vec3 ObjectColor;
-uniform vec3 LightColor;
-uniform vec3 LightPos;
-uniform vec3 ViewPos;
-
 
 void main()
 {
+	Normal = mat3(transpose(inverse(Model)))*inNormal;
+	FragPos = vec3(Model*vec4(aPos, 1.0));
+
 	gl_Position = Projection*View*Model*vec4(aPos, 1.0);
-
-	LightPosInViewSpace = vec3(View*vec4(LightPos, 1.0));
-
-	Normal = mat3(transpose(inverse(View*Model)))*inNormal;
-	FragPos = vec3(View*Model*vec4(aPos, 1.0));
-
-	    // cal diffuse
-    vec3 NormalizeNormal = normalize(Normal);
-    vec3 LightDir = normalize(LightPosInViewSpace-FragPos);
-    float DiffuseFactor = max(dot(NormalizeNormal, LightDir), 0.0);
-    vec3 Diffuse = DiffuseFactor*LightColor;
-
-    // cal specular
-    vec3 ViewDir = normalize(-FragPos);
-    vec3 LightReflectDir = reflect(-LightDir, NormalizeNormal);
-    float Spec = pow(max(dot(ViewDir, LightReflectDir), 0.0), 32);
-    vec3 Specular = SpecularStrength*Spec*LightColor;
-
-
-
-	// cal ambient
-    vec3 Ambint = LightColor*AmbientStrength;
-
-	LightingColor = Ambint+Diffuse+Specular;
 }
