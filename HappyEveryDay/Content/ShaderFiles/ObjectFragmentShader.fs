42f710eb34ada589dc538a85d0ff64b7bc41f317
dufresnefu <dufresnefu@tencent.com>
2022-12-09 12:15:49 +0800

    在顶点着色器(VertexShader)中计算光照

--- HappyEveryDay/Content/ShaderFiles/ObjectFragmentShader.fs
+++ HappyEveryDay/Content/ShaderFiles/ObjectFragmentShader.fs
@@ -3,6 +3,7 @@
 in vec3 Normal;  
 in vec3 FragPos;  
 in vec3 LightPosInViewSpace;  
+in vec3 LightingColor; 
 
 
 out vec4 FragColor;
@@ -21,20 +22,21 @@ uniform mat4 View;
 
 void main()
 {
-    // cal ambient
-    vec3 Ambint = LightColor*AmbientStrength;
+    //// cal ambient
+    //vec3 Ambint = LightColor*AmbientStrength;
+//
+    //// cal diffuse
+    //vec3 NormalizeNormal = normalize(Normal);
+    //vec3 LightDir = normalize(LightPosInViewSpace-FragPos);
+    //float DiffuseFactor = max(dot(NormalizeNormal, LightDir), 0.0);
+    //vec3 Diffuse = DiffuseFactor*LightColor;
+//
+    //// cal specular
+    //vec3 ViewDir = normalize(-FragPos);
+    //vec3 LightReflectDir = reflect(-LightDir, NormalizeNormal);
+    //float Spec = pow(max(dot(ViewDir, LightReflectDir), 0.0), 32);
+    //vec3 Specular = SpecularStrength*Spec*LightColor;
 
-    // cal diffuse
-    vec3 NormalizeNormal = normalize(Normal);
-    vec3 LightDir = normalize(LightPosInViewSpace-FragPos);
-    float DiffuseFactor = max(dot(NormalizeNormal, LightDir), 0.0);
-    vec3 Diffuse = DiffuseFactor*LightColor;
-
-    // cal specular
-    vec3 ViewDir = normalize(-FragPos);
-    vec3 LightReflectDir = reflect(-LightDir, NormalizeNormal);
-    float Spec = pow(max(dot(ViewDir, LightReflectDir), 0.0), 32);
-    vec3 Specular = SpecularStrength*Spec*LightColor;
-
-    FragColor = vec4((Ambint+Diffuse+Specular) * ObjectColor, 1.0);
+    //FragColor = vec4((Ambint+Diffuse+Specular) * ObjectColor, 1.0);
+    FragColor = vec4(LightingColor* ObjectColor, 1.0);
 }
